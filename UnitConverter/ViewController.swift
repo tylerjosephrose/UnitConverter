c09789dc1497e8a8d28ad34bbca5495061d4dadb
Tyler Rose <tylerjosephrose@gmail.com>
2017-02-07 12:09:50 -0500

    Picker with all unit names

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		loadUnits()
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	var unitConversions = [String: Float]()
+	var units = [String]()
+	
+	func loadUnits() {
+		let filepath = Bundle.main.path(forResource: "Length", ofType: "plist")
+		unitConversions = NSDictionary(contentsOfFile: filepath!) as! Dictionary<String, Float>
+		for item in unitConversions {
+			units.append(item.key)
+		}
+	}
 
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return units.count
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		return units[row]
+	}
 
 }
 

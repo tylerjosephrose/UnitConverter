c624c2000025712d065f0551027da8a0a00810d7
Tyler Rose <tylerjosephrose@gmail.com>
2017-02-13 13:16:45 -0500

    Added conversion

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -14,15 +14,27 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		loadUnits()
+		updateDisplay()
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
+	@IBOutlet weak var unitsLabel: UILabel!
+	@IBOutlet weak var outputLabel: UILabel!
 	var unitConversions = [String: Float]()
 	var units = [String]()
+	var convertedAmount: Float = 0.0
+	@IBOutlet weak var inputValue: UITextField!
+	@IBOutlet weak var fromPicker: UIPickerView!
+	@IBOutlet weak var toPicker: UIPickerView!
+	
+	@IBAction func convertPressed(_ sender: UIButton) {
+		convertUnits()
+	}
+	
+	func updateDisplay() {
+		unitsLabel.text = units[toPicker.selectedRow(inComponent: 0)]
+		outputLabel.text = String(convertedAmount)
+		
+	}
 	
 	func loadUnits() {
 		let filepath = Bundle.main.path(forResource: "Length", ofType: "plist")
@@ -35,6 +47,38 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 	
 	func sortUnits(first: (key: String, value: Float), second: (key: String, value: Float)) -> Bool {
 		return first.value > second.value
+	}
+	
+	func convertUnits()
+	{
+		let numberFormatter = NumberFormatter()
+		// get out if no number was entered
+		if inputValue.text == ""
+		{
+			updateDisplay()
+			return
+		}
+		// if an invalid number is entered, alert the user
+		if (numberFormatter.number(from: inputValue.text!)?.floatValue) == nil {
+			let alertController = UIAlertController(title: "Invalid Amount", message: "Please enter a valid amount", preferredStyle: .alert)
+			let action = UIAlertAction(title: "Okay", style: .default, handler: nil)
+			alertController.addAction(action)
+			present(alertController, animated: true, completion: nil)
+			updateDisplay()
+			return
+		}
+		// get amount
+		let input = numberFormatter.number(from: inputValue.text!)?.floatValue
+		// get from unit conversion
+		let fromUnitConversion = unitConversions[units[fromPicker.selectedRow(inComponent: 0)]]!
+		// convert to meters
+		let inputInMeters = input! * fromUnitConversion
+		// get to unit conversion
+		let toUnitConversion = unitConversions[units[toPicker.selectedRow(inComponent: 0)]]!
+		// convert to to unit
+		convertedAmount = Float(inputInMeters / toUnitConversion)
+		inputValue.resignFirstResponder()
+		updateDisplay()
 	}
 
 	func numberOfComponents(in pickerView: UIPickerView) -> Int {
